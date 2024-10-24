98249b4f9cf2a2a2b38d8861c97b845bc40ba471
Vigneshwar R <105167539+Vigneshwarr3@users.noreply.github.com>
2024-10-23 21:55:32 -0400

    init file

--- prompt.py
+++ prompt.py
@@ -1,11 +1,4 @@
 def report_count(token):
-    try:
-        with open('corpus.txt', 'r') as file:
-            corpus = file.read()
-
-        count = corpus.lower().split().count(token.lower())
-
-        print(f"The term {token} shows up in the corpus {count} times.")
-    
-    except FileNotFoundError:
-        print("The corpus.txt file was not found.")
+    corpus = read_corpus(filepath)
+    count = count_token_occurrences(corpus, token)
+    print(f"The term [{token}] shows up in the corpus {count} times.")
